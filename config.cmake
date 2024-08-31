f7f62e77f19f6ea21fd7f57a3bd20eb7251cc634
billowqiu <billowqiu@163.com>
2024-08-31 12:33:39 +0800

    update cmake

---
+++ config.cmake
@@ -0,0 +1,17 @@
+hunter_config(
+    Boost 
+    VERSION 
+    1.70.0-p0
+)
+
+hunter_config(
+    opentracing-cpp
+    VERSION 
+    1.5.0
+)
+
+hunter_config(
+    GTest 
+    VERSION 
+    1.8.0-hunter-p11
+)
